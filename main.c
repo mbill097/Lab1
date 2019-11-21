4ec7048853409d8d11580f81d39780df7907cc68
mbill <m.billupalli@eagles.oc.edu>
2019-11-20 21:31:16 -0600

    version 1.0

--- main.c
+++ main.c
@@ -8,7 +8,7 @@ int rand(int r0, int a, int m, int c){
     double r1=(a*r0+c)%m;
     return r1;
 }
-void randomNos(int r0, int a, int m, int c, int n, int x[n]){
+void randNos(int r0, int a, int m, int c, int n, int x[n]){
     double r1=rand(r0,a,m,c);
     int i;
     for(i=0;i<n;i++){
@@ -16,7 +16,7 @@ void randomNos(int r0, int a, int m, int c, int n, int x[n]){
         r1=rand(r1,a,m,c);
     }
 }
-void randomNosRange(int n, double r[n], double x[n], double min, double max){
+void randNosRange(int n, double r[n], double x[n], double min, double max){
     int i;
     double r1;
     for(i=0;i<n;i++){
@@ -30,17 +30,16 @@ long double Calculate_Pi_Sequential(long n)
     int min=-1, max=1, a=1093, m=86436, c=18257;
     double x0=43,y0=21;
     int i,j,k,l,xtemp[n],ytemp[n];
-	randomNos(x0,a,m,c,n,xtemp);
-    randomNos(y0,a,m,c,n,ytemp);
+	randNos(x0,a,m,c,n,xtemp);
+    randNos(y0,a,m,c,n,ytemp);
     double xtemp1[n],ytemp1[n];
     for(i=0;i<n;i++){
         xtemp1[i]=(double)xtemp[i]/(m-1);
         ytemp1[i]=(double)ytemp[i]/(m-1);
     }
-    double x[n];    //x-coordinates
-    double y[n];    //y-coordinates
-    randomNosRange(n,xtemp1,x,-1,1);
-    randomNosRange(n,ytemp1,y,-1,1);
+    double x[n],y[n];
+    randNosRange(n,xtemp1,x,-1,1);
+    randNosRange(n,ytemp1,y,-1,1);
     int Naccept=0;
     for(i=0;i<n;i++){
         double s=pow(x[i],2)+pow(y[i],2);
@@ -58,16 +57,16 @@ long double Calculate_Pi_Parallel(long n)
     int min=-1, max=1, a=1093, m=86436, c=18257;
     double x0=43,y0=21;
     int i,j,k,l,xtemp[n],ytemp[n];
-    randomNos(x0,a,m,c,n,xtemp);
-    randomNos(y0,a,m,c,n,ytemp);
+    randNos(x0,a,m,c,n,xtemp);
+    randNos(y0,a,m,c,n,ytemp);
     double xtemp1[n],ytemp1[n];
     for(i=0;i<n;i++){
         xtemp1[i]=(double)xtemp[i]/(m-1);
         ytemp1[i]=(double)ytemp[i]/(m-1);
     }
     double x[n],y[n];
-    randomNosRange(n,xtemp1,x,-1,1);
-    randomNosRange(n,ytemp1,y,-1,1);
+    randNosRange(n,xtemp1,x,-1,1);
+    randNosRange(n,ytemp1,y,-1,1);
     int Naccept=0;
     for(i=0;i<n;i++){
         double s=pow(x[i],2)+pow(y[i],2);
